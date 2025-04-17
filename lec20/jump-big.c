0e8a04b8a4301b9f519edf3b9944575b091c7230
Anna Arpaci-Dusseau <anna.arpacidusseau@gmail.com>
2025-04-17 08:48:31 -0400

    lec20

---
+++ lec20/jump-big.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10000000
+
+int main(void) {
+  int* A = malloc(sizeof(int) * SIZE);
+  
+  for(int i = 0; i < SIZE; i++) {
+    A[rand() % SIZE] = rand();
+  }
+
+  int sum = 0;
+  for(int i = 0; i < SIZE; i++) {
+    sum += A[rand() % SIZE];
+  }
+
+  printf("sum: %d\n", sum);
+}
---
+++ lec20/sequential-big.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10000000
+
+int main(void) {
+  int* A = malloc(sizeof(int) * SIZE);
+  
+  for(int i = 0; i < SIZE; i++) {
+    A[i] = rand() + rand();
+  }
+
+  int sum = 0;
+  for(int i = 0; i < SIZE; i++) {
+    sum += rand() + A[i];
+  }
+
+  printf("sum: %d\n", sum);
+}
